bce3dbd1cbfd43f651c767725396badb0d2e0e1c
Hermes Passer <dsldouglas@hotmail.com>
2021-11-02 14:01:12 -0300

    Initial commit

---
+++ WebSearch.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+class WebSearch
+{
+	static void Main(string[] args)
+	{
+		String search = String.Join('+', args);
+		String url = $"https://duckduckgo.com/?q={search}&t=h_&ia=web";
+		
+		Process proc = new Process();
+		proc.StartInfo.FileName = "cmd.exe";
+		proc.StartInfo.Arguments = $" /c start {url}";
+		proc.StartInfo.UseShellExecute = false;
+		proc.StartInfo.CreateNoWindow = true;
+		proc.Start();
+		
+		System.Console.WriteLine(url);
+	}
+}
